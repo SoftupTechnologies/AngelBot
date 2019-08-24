35a2094865a0a2fc526ebca09e42a6d124beac4a
Erald Totraku <erald.totraku@gmail.com>
2019-08-24 14:22:20 +0200

    slack token verification

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const parseInput = require('./parse_changelog');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware');
+require('dotenv').config();
 
 // TODO send the data to response_url of the slack request
 // https://api.slack.com/slash-commands#responding_response_url
@@ -12,7 +12,6 @@ const awsServerlessExpressMiddleware = require('aws-serverless-express/middlewar
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(awsServerlessExpressMiddleware.eventContext());
 
 // func is an async function and e.g. does CRUD operations
 const handleFunc = (func, res) => {
@@ -26,61 +25,92 @@ const handleFunc = (func, res) => {
     .catch((error) => {
       return res.status(400).send({
         success: 'false',
-        message: JSON.parse(error)
+        message: error
       });
     });
+};
+
+const checkIdentity = (req, res) => {
+  if (process.env.SLACK_APP_VERIFICATION_TOKEN === req.body.token) {
+    return (true);
+  }
+  return false;
+};
+
+const respondUnauthorized = (res) => {
+  return res.status(401).send({
+    success: 'false',
+    message: 'Wrong or missing slack verfication token'
+  });
 };
 
 // post all changelogs (same version will be overwritten) or
 // post the changelog which is on the top, which usually means the newest
 app.post('/api/v1/changelog_write', (req, res) => {
-  if (!req.body.content) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'content is required'
-    });
-  }
-  const content = req.body.content;
-  const batchStore = req.body.store_all;
-  try {
-    const parsed = parseInput(content);
-    if (batchStore) {
-      handleFunc(dbAction.batchStoreChangelog(parsed), res);
-    } else {
-      handleFunc(dbAction.storeChangelog(parsed), res);
+  if (checkIdentity(req, res)) {
+    if (!req.body.content) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'content is required'
+      });
     }
-  } catch (error) {
-    return res.status(400).send({
-      success: 'false',
-      message: error.toString()
-    });
+    const content = req.body.content;
+    const batchStore = req.body.store_all;
+    try {
+      const parsed = parseInput(content);
+      if (batchStore) {
+        handleFunc(dbAction.batchStoreChangelog(parsed), res);
+      } else {
+        handleFunc(dbAction.storeChangelog(parsed), res);
+      }
+    } catch (error) {
+      return res.status(400).send({
+        success: 'false',
+        message: error.toString()
+      });
+    }
+  } else {
+    respondUnauthorized(res);
   }
 });
 
 // Get all changelogs or a specific changelog by passing the version
 app.post('/api/v1/changelog_read', (req, res) => {
-  if (req.body.version) {
-    const version = req.body.version;
-    handleFunc(dbAction.readChangelog(version), res);
+  if (checkIdentity(req, res)) {
+    if (req.body.version) {
+      const version = req.body.version;
+      handleFunc(dbAction.readChangelog(version), res);
+    } else {
+      handleFunc(dbAction.readChangelog(), res);
+    }
   } else {
-    handleFunc(dbAction.readChangelog(), res);
+    respondUnauthorized(res);
   }
 });
 
 // Get changes from a specific category by specifying it in the request
 app.post('/api/v1/changelog/category_changes', (req, res) => {
-  if (!req.body.category) {
-    return res.status(400).send({
-      success: 'false',
-      message: 'category is required'
-    });
+  if (checkIdentity(req, res)) {
+    if (!req.body.category) {
+      return res.status(400).send({
+        success: 'false',
+        message: 'category is required'
+      });
+    }
+    const category = req.body.category;
+    handleFunc(dbAction.readCategoryChanges(category), res);
+  } else {
+    respondUnauthorized(res);
   }
-  const category = req.body.category;
-  handleFunc(dbAction.readCategoryChanges(category), res);
 });
 
 app.post('/api/v1/init', (req, res) => {
-  handleFunc(dbAction.createChangelogTable(), res);
+  if (checkIdentity(req, res)) {
+    checkIdentity(req, res);
+    handleFunc(dbAction.createChangelogTable(), res);
+  } else {
+    respondUnauthorized(res);
+  }
 });
 
 module.exports = app;
