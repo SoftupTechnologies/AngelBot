5ad329a5c0cc68048eac77e4cf3807b2497ea1b9
Erald Totraku <erald.totraku@gmail.com>
2019-09-04 15:03:38 +0200

    added required packages, adjusted local entrypoint

--- dealer-write/app.local.js
+++ dealer-write/app.local.js
@@ -1,7 +1,6 @@
 const app = require('./app');
+require('dotenv').config();
 
-const PORT = 8000;
-
-app.listen(PORT, () => {
-  console.log(`App is running on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App is running on port ${process.env.PORT}`);
 });
